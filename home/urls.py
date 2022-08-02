9fb692d376b2c82eb39b5256cee8cf096b840310
Tiff-C <deguntiffany@gmail.com>
2022-08-02 13:12:47 +0000

    Initial Commit, req apps copied from outreach-rms

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+
+"""
+Import required modules for home app urls
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+]
---
+++ home/views.py
@@ -0,0 +1,12 @@
+""" Imports for home app views """
+from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+# @login_required
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')
